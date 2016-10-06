14d30d24a3b3d558eb1becae37b729126961020f
IlincaM <m.ilinca92@gmail.com>
2016-10-06 15:34:08 +0300

    fixed sources

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -17,12 +17,12 @@
     <script type="text/javascript" src="//code.jquery.com/jquery-2.2.1.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript"></script>
 
-    <script type="text/javascript" src=" {{ URL::asset('../public/js/jquery.easing.1.3.js') }}"></script>
+    <script type="text/javascript" src=" {{ URL::asset('js/jquery.easing.1.3.js') }}"></script>
 
-    <link rel="stylesheet" href="{{ URL::asset('../public/css/styleCarousel.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ URL::asset('css/styleCarousel.css') }}" type="text/css" />
 
-    <script type="text/javascript" src="{{ URL::asset('../public/js/style.js') }}"></script>
-    <script type="text/javascript" src="{{ URL::asset('../public/js/menu.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('js/style.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('js/menu.js') }}"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
