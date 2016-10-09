cbcbacea6884fa4768182a88cbc3d09782543d3f
IlincaM <m.ilinca92@gmail.com>
2016-10-09 12:29:04 +0300

    changes

--- resources/views/pages/aboutMe.blade.php
+++ resources/views/pages/aboutMe.blade.php
@@ -5,10 +5,14 @@
 
 @section('content')
 <div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
 <h1 class="subtitle">Get to know me</h1>
-
-    <div class=" blog"
+<div class="blog">   
          <p>I am a young, highly passionate web developer, with good understanding of programming principles, and with a very big desire to become better and better each day.</p>    
-    </div> 
+    
+</div>
+</div>
+        </div>
 </div>
 @endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -4,12 +4,15 @@
 
 @section('content')
 <div class="container">
-
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
 <h1 class="subtitle">Hello Beauty</h1>
 <div class="blog">   
     <p>This is a fashion blog. Here you can find help concerning your capillary jewely. Section "Hair tutorials"  comes to help with solutions such as using homemade hair masks rich in nutrients. 
         Also in this section you can get your inspiration for your occasions with perfect hairstyles</p>    
     
 </div>
+</div>
+        </div>
 </div>
 @endsection
