8e716e7c0717ef6f249f97606d4c567cd45a7a65
IlincaM <m.ilinca92@gmail.com>
2016-10-12 07:31:01 +0300

    upload img

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,9 @@ use Session;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Image;
+use Faker\Provider\Image;
+use Intervention\Image\Facades\Image;
+
 class PostController extends Controller {
 
     /**
